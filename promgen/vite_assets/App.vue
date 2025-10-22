e9f72d15fe80b3c78ff4bd330f93c37bda38db0e
Vicente Olivert Riera <vincent.olivert.riera@linecorp.com>
2025-10-22 20:14:32 +0900

    vite: Add i18n

--- promgen/vite_assets/App.vue
+++ promgen/vite_assets/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Promgen</div>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale"
+      >
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+  <h1>{{ $t("msg") }}</h1>
 </template>
