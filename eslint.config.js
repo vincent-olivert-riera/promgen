c1df5fa4f2b31cc80df97b8be918c8dc7d50f7b1
Vicente Olivert Riera <vincent.olivert.riera@linecorp.com>
2025-10-22 18:39:43 +0900

    vite: Add prettier
    We cannot specify the css file extension for eslint, therefore we need to run
    prettier separately if we want to lint the css files.
    
    When formatting, prettier will format kind of files.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import gitignore from "eslint-config-flat-gitignore";
 import pluginVue from "eslint-plugin-vue";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 
 
 export default [
   gitignore(),
   ...pluginVue.configs["flat/recommended"],
+  pluginPrettier,
   {
     rules: {
       "vue/max-attributes-per-line": "off",
