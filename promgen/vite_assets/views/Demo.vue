db1826d3fc9461c27be62eae55e8943bb0244b9d
Vicente Olivert Riera <vincent.olivert.riera@linecorp.com>
2025-10-23 17:45:23 +0900

    vite: Add pinia

--- promgen/vite_assets/views/Demo.vue
+++ promgen/vite_assets/views/Demo.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useDemoStore } from "../stores/demo.js";
+
+const demoStore = useDemoStore();
+</script>
+
 <template>
   <div class="locale-changer">
     <select v-model="$i18n.locale">
@@ -11,4 +17,10 @@
     </select>
   </div>
   <h1>{{ $t("msg") }}</h1>
+
+  <ul class="store-demo">
+    <li v-for="(fruit, index) in demoStore.fruits" :key="index">
+      {{ $t(fruit) }}
+    </li>
+  </ul>
 </template>
